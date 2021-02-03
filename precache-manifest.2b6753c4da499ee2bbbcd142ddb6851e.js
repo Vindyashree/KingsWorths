758f6518cfa802ca2e178315833f7e30802475b3
Vindyashree S <Vindyashree@cloudthing.com>
2021-02-03 23:48:30 +0530

    Updates

---
+++ precache-manifest.2b6753c4da499ee2bbbcd142ddb6851e.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8c6ccb2b329eea1d2b387db3c4550bc",
+    "url": "/Vindyashree/KingsWorths/index.html"
+  },
+  {
+    "revision": "4d8bce308a88c974b6d4",
+    "url": "/Vindyashree/KingsWorths/static/css/main.c5a3ee5c.chunk.css"
+  },
+  {
+    "revision": "33e51ec348f059b0df13",
+    "url": "/Vindyashree/KingsWorths/static/js/2.87e0b579.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/Vindyashree/KingsWorths/static/js/2.87e0b579.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d8bce308a88c974b6d4",
+    "url": "/Vindyashree/KingsWorths/static/js/main.330e057a.chunk.js"
+  },
+  {
+    "revision": "c673ee81685f1b7de7a8",
+    "url": "/Vindyashree/KingsWorths/static/js/runtime-main.8e7796d3.js"
+  },
+  {
+    "revision": "f1d71f777331fd7e3de116edf4ee3b67",
+    "url": "/Vindyashree/KingsWorths/static/media/avatar.f1d71f77.jpg"
+  },
+  {
+    "revision": "e52419719dd0c4c8b172136f0b625008",
+    "url": "/Vindyashree/KingsWorths/static/media/bg.e5241971.jpg"
+  },
+  {
+    "revision": "843781834b1db2908f08d921c6460c81",
+    "url": "/Vindyashree/KingsWorths/static/media/bg2.84378183.jpg"
+  },
+  {
+    "revision": "e76de24b6d8ab5e920bb4d49241ad15f",
+    "url": "/Vindyashree/KingsWorths/static/media/bg3.e76de24b.jpg"
+  },
+  {
+    "revision": "b08234a2b7e8e4fb2e3c4e713252021c",
+    "url": "/Vindyashree/KingsWorths/static/media/bg7.b08234a2.jpg"
+  },
+  {
+    "revision": "56633ed3f62f39d71f571374a6409e65",
+    "url": "/Vindyashree/KingsWorths/static/media/christian.56633ed3.jpg"
+  },
+  {
+    "revision": "c058841b1dd64e5f79c6348b24cfb78d",
+    "url": "/Vindyashree/KingsWorths/static/media/clem-onojegaw.c058841b.jpg"
+  },
+  {
+    "revision": "216ef03c54bc13771c5e1b8d8f8d5926",
+    "url": "/Vindyashree/KingsWorths/static/media/clem-onojeghuo.216ef03c.jpg"
+  },
+  {
+    "revision": "9813593cc577a319a2201342ef0fb237",
+    "url": "/Vindyashree/KingsWorths/static/media/cynthia-del-rio.9813593c.jpg"
+  },
+  {
+    "revision": "e0508cc923eb0b5e68ca6783c53d0f1d",
+    "url": "/Vindyashree/KingsWorths/static/media/kendall.e0508cc9.jpg"
+  },
+  {
+    "revision": "f09f57b81275967a0991afdbae0647fc",
+    "url": "/Vindyashree/KingsWorths/static/media/kingbg.f09f57b8.jpg"
+  },
+  {
+    "revision": "df8fd3efcd662b64b44de07f351c838b",
+    "url": "/Vindyashree/KingsWorths/static/media/landing-bg.df8fd3ef.jpg"
+  },
+  {
+    "revision": "08e18cb904f0f3c6dd9e3d4ed72e0eca",
+    "url": "/Vindyashree/KingsWorths/static/media/landing.08e18cb9.jpg"
+  },
+  {
+    "revision": "4575c40bfb8dec6713d2da51c4c4066e",
+    "url": "/Vindyashree/KingsWorths/static/media/mariya-georgieva.4575c40b.jpg"
+  },
+  {
+    "revision": "4112cbc1477d9e149033c5df66087e16",
+    "url": "/Vindyashree/KingsWorths/static/media/olu-eletu.4112cbc1.jpg"
+  },
+  {
+    "revision": "baf6b40a654b078399e93e3d9cb6d455",
+    "url": "/Vindyashree/KingsWorths/static/media/profile-bg.baf6b40a.jpg"
+  },
+  {
+    "revision": "090a5aabae505f67ee0981613d02ee05",
+    "url": "/Vindyashree/KingsWorths/static/media/profile.090a5aab.jpg"
+  },
+  {
+    "revision": "ae0150c08dbcc95e4f50458e02e5bd5c",
+    "url": "/Vindyashree/KingsWorths/static/media/studio-1.ae0150c0.jpg"
+  },
+  {
+    "revision": "76e2987ed95634136dd22d4d9e1009a7",
+    "url": "/Vindyashree/KingsWorths/static/media/studio-2.76e2987e.jpg"
+  },
+  {
+    "revision": "1d5451ced89eabb55683e27e070bdb60",
+    "url": "/Vindyashree/KingsWorths/static/media/studio-3.1d5451ce.jpg"
+  },
+  {
+    "revision": "e064d0908dbd53b55f8980c02b3748bb",
+    "url": "/Vindyashree/KingsWorths/static/media/studio-4.e064d090.jpg"
+  },
+  {
+    "revision": "ef5c30ea69b7ad740ee6221782c73741",
+    "url": "/Vindyashree/KingsWorths/static/media/studio-5.ef5c30ea.jpg"
+  }
+]);
